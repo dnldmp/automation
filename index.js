51ff2d7f26d8223cf6733432e4c6fc6b972c1458
Danilo Dominoni <danilo.paula@qintess.com>
2022-12-09 17:16:00 -0300

    provide information related to params

--- index.js
+++ index.js
@@ -1,18 +1,48 @@
 const express = require("express");
 const cors = require("cors");
-var bodyParser = require("body-parser");
-const open = require("open");
+const bodyParser = require("body-parser");
+const url = require("url");
+const queryString = require("query-string");
+const { default: axios } = require("axios");
+const puppeteer = require("puppeteer-core");
+const { executablePath } = require("puppeteer");
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json({ type: "application/json" }));
 
-app.post("/api", (req, res) => {
+app.post("/api", async (req, res) => {
   const { link } = req.body;
 
-  open(link);
+  if (!link) return res.send("no link");
 
-  res.send("accepted");
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox"],
+    headless: true,
+    ignoreHTTPSErrors: true,
+
+    // add this
+    executablePath: executablePath(),
+  });
+  const page = await browser.newPage();
+
+  await page.goto(link);
+  await page.waitForSelector(".spinner");
+  await browser.close();
+
+  res.send("sucesss");
+
+  // const urlparsed = url.parse(link);
+  // const { payment_intent, publishable_key, payment_intent_client_secret } =
+  //   queryString.parse(urlparsed.query);
+
+  // axios
+  //   .get(
+  //     `https://api.stripe.com/v1/payment_intents/${payment_intent}?key=${publishable_key}&is_stripe_sdk=true&client_secret=${payment_intent_client_secret}`
+  //   )
+  //   .then((response) => {
+  //     res.send(response.data);
+  //   });
 });
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 3000);
